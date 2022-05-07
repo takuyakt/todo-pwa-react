c853a6cd8459caf3c4e75d3f5ccc094ccf350858
takuyakt <10061798+takuyakt@users.noreply.github.com>
2022-05-07 22:14:32 +0900

    add sample method to add contents to a todo list.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,19 @@
 import React, {useState, useEffect, useContext} from "react" ;
+import * as Api from "../service/api"
 import { signInWithGoogle } from "../service/firebase";
 import dig from 'object-dig';
-import { AuthContext } from "../providers/AuthProvider";
+import AuthProvider, { AuthContext } from "../providers/AuthProvider";
 
 const Dashboard = () => {
     const currentUser = useContext(AuthContext);
+    const [inputName, setInputName ] = useState("");
+    console.log(inputName);
     const formRender = () => {
         let dom
         if( dig(currentUser,'currentUser', 'uid' ) ){
             dom = <form>
-                <input placeholder="ToDoName" />
-                <button>追加</button>
+                <input placeholder="ToDoName" value={inputName} onChange={(event) => setInputName(event.currentTarget.value)}/>
+                <button type="button" onClick={() => post()}>追加</button>
             </form>
         }else{
             //ログインしていない場合
@@ -18,6 +21,12 @@ const Dashboard = () => {
         }
         return dom
     }
+
+    const post = () => {
+        Api.addTodo(inputName, currentUser.currentUser.uid);
+        setInputName("");
+    }
+
 
     return(
         <div>
